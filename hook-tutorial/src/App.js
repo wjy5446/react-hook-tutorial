53cc7b2c0f9bc4cb2dce3f2e5957bfda88ba30df
wjy5446 <wjy5446@gmail.com>
2020-09-03 22:47:20 +0900

    feat: all

--- hook-tutorial/src/App.js
+++ hook-tutorial/src/App.js
@@ -1,26 +1,55 @@
+import defaultAxios from "axios";
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
-const usePreventLeave = () => {
-  const listener = (event) => {
-    event.preventDefault();
-    event.returnValue = "";
+const useAxios = (opts, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null,
+  });
+
+  const [trigger, setTrigger] = useState("");
+
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    console.log(state);
+    setTrigger(Date.now());
   };
 
-  const enablePrevent = () => window.addEventListener("beforeunload", listener);
-  const disablePrevent = () =>
-    window.removeEventListener("beforeunload", listener);
-
-  return { enablePrevent, disablePrevent };
+  useEffect(() => {
+    axiosInstance(opts)
+      .then((data) => {
+        setState({
+          ...state,
+          loading: false,
+          data,
+        });
+      })
+      .catch((error) => {
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+  return { ...state, refetch };
 };
 
 function App() {
-  const { protect, unprotect } = usePreventLeave();
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.am/api/v2/list_movies.json",
+  });
+
+  console.log("q");
+  console.log(refetch);
+  console.log(loading, data, error);
 
   return (
-    <div className="App">
-      <button onClick={protect}>Protect</button>
-      <button onClick={unprotect}>Unprotext</button>
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 }
--- hook-tutorial/src/hook/modules.js
+++ hook-tutorial/src/hook/modules.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 // useInput : input에 들어갈 값을 설정한다. validator을 통해서 입력될 값을 확인할 수 있다.
 export const useInput = (initialValue, validator) => {
@@ -87,4 +87,152 @@ export const usePreventLeave = () => {
     window.removeEventListener("beforeunload", listener);
 
   return { enablePrevent, disablePrevent };
+};
+
+// useBeforeLeave
+const useBeforeLeave = (onBefore) => {
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => document.removeEventListener("mouseleave", handle);
+  }, []);
+
+  if (typeof onBefore !== "function") {
+    return;
+  }
+
+  const handle = () => {
+    onBefore();
+  };
+};
+
+// useFaceIn
+const useFaceIn = (duration, delay = 0) => {
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  }, []);
+
+  return { ref: element, style: { opacity: 0 } };
+};
+
+// useNetwork
+const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+
+  const handleChange = () => {
+    onChange(navigator.onLine);
+    setStatus(navigator.onLine);
+  };
+
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+
+    return () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
+  });
+
+  return status;
+};
+
+//useScroll
+const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const onScroll = (event) => {
+    setState({ y: window.scrollY, x: window.scrollX });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  });
+
+  return state;
+};
+
+// useFullscreen
+const useFullscreen = (callback) => {
+  const element = useRef();
+
+  const triggerFull = () => {
+    if (element.current) {
+      element.current.requestFullscreen();
+      callback(true);
+    }
+  };
+
+  const exitFull = () => {
+    document.exitFullscreen();
+    callback(false);
+  };
+  return { element, triggerFull, exitFull };
+};
+
+// useNotification
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+
+  const fireNotification = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    }
+  };
+
+  return fireNotification;
+};
+
+// useAxios
+const useAxios = (opts, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null,
+  });
+
+  const [trigger, setTrigger] = useState("");
+
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    console.log(state);
+    setTrigger(Date.now());
+  };
+
+  useEffect(() => {
+    axiosInstance(opts)
+      .then((data) => {
+        setState({
+          ...state,
+          loading: false,
+          data,
+        });
+      })
+      .catch((error) => {
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+  return { ...state, refetch };
 };
